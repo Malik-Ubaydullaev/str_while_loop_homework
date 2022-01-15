e0cbafcd5019b031e83d81996ac8cad24ce3f257
Your Name <ubaydullayev@samtuit.uz>
2022-01-15 15:06:56 +0500

    3-misol

--- While03.py
+++ While03.py
@@ -1,3 +1,8 @@
+def count_punctuation(ch):
+    if ch == ',' or ch == '.' or ch == ';' or ch == ':' or ch == '?' or ch == '!':
+        return 1
+    else:
+        return 0    
 def main(s):
     """
     A variable of type str is given. Find how many punctuations it contains and return.
@@ -6,4 +11,9 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx = 0
+    count_punct = 0
+    while idx < len(s):
+        count_punct += count_punctuation(s[idx])
+        idx += 1
+    return count_punct

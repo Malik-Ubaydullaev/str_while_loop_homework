68b0f18f24559e172006debd2b0997402b85ff0d
Your Name <ubaydullayev@samtuit.uz>
2022-01-15 15:34:33 +0500

    6-misol

--- While06.py
+++ While06.py
@@ -1,3 +1,9 @@
+def count_consonant(ch):
+    if ch == 'A' or ch == 'a' or ch == 'E' or ch == 'e' or ch == 'I' or ch == 'i' or ch == 'O' or ch == 'o' or ch == 'U' or ch == 'u' or ch == 'Y' or ch == 'y':
+        return 1
+    else:
+        return 0
+
 def main(s):
     """
     A variable of type str is given. Find and return how many consonant letters there are.
@@ -7,4 +13,9 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx = 0
+    count_cons = 0
+    while idx < len(s):
+        count_cons += count_consonant(s[idx])
+        idx += 1
+    return count_cons

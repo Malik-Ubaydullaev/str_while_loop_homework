cc31cdd1fe6a17bee78d4f876bf8089458d2900c
Your Name <ubaydullayev@samtuit.uz>
2022-01-15 15:22:19 +0500

    7-misol

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count_evenNumber = 0
+    idx = 0
+    while idx < len(s):
+        if s[idx].isdigit():
+            if int(s[idx]) % 2 == 0:
+                count_evenNumber += 1
+        idx += 1
+    return count_evenNumber

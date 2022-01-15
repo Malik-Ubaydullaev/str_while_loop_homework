103ae6158b91d32286a502112da248958a13996d
Your Name <ubaydullayev@samtuit.uz>
2022-01-15 14:54:06 +0500

    2-misol

--- While02.py
+++ While02.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count_alpha = 0
+    idx = 0
+    while idx < len(s):
+        if s[idx].isalpha():
+            count_alpha += 1
+        idx += 1
+    return count_alpha
